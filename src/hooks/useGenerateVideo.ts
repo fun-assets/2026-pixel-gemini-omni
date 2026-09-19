c40baf1afd6cd735328f3c950edf00de55f92774
jameshsu1125 <jameshsu1125@gmail.com>
2026-09-19 15:11:57 +0800

    移除影片生成過程中的日誌輸出

--- src/hooks/useGenerateVideo.ts
+++ src/hooks/useGenerateVideo.ts
@@ -60,8 +60,6 @@ const useGenerateVideo = () => {
   const fetch = async (argument: TArgument) => {
     try {
       const respond = (await Fetcher.post(REST_PATH.generateVideo, argument)) as TVideoResponse;
-      console.log(respond);
-
       if (respond.res) setState(respond);
       else {
         if (respond.msg.includes('版權')) {
